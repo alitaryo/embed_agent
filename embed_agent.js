c6b52ea5fd6f69a687a23701e4dda2f0d3569058
Alita Health Ryo <160247467+alitaryo@users.noreply.github.com>
2024-10-03 17:01:48 -0400

    Update embed_agent.js

--- embed_agent.js
+++ embed_agent.js
@@ -5,6 +5,10 @@
 
       const AppId = this.getAttribute("bbuilder-app-id");  
       const chatBubble = this.getAttribute("chat-bubble");  
+      const minWidth = this.getAttribute("min-width");  
+      const minHeight = this.getAttribute("min-height");  
+      const maxWidth = this.getAttribute("max-width");  
+      const maxHeight = this.getAttribute("max-height");  
 
       const iframe = document.createElement("iframe");  
 
@@ -13,17 +17,30 @@
         "src",  
         `${this.getAttribute("host") || "https://app.alitahealth.ai/"}/agent/${AppId}`  
       );  
-      iframe.setAttribute("width", "100%");  
-      iframe.setAttribute("height", "80vh"); // Default height  
-      iframe.setAttribute("scrolling", "no");  
-      iframe.setAttribute("frameborder", "0");  
+      iframe.setAttribute("width", this.getAttribute("width") || "100%");  
+      iframe.setAttribute("height", this.getAttribute("height") || "80vh");  
+      iframe.setAttribute("scrolling", this.getAttribute("scrolling") || "no");  
+      iframe.setAttribute("frameborder", this.getAttribute("frameborder") || "0");  
+      iframe.setAttribute("allow", "clipboard-read; clipboard-write");  
 
       if (chatBubble) {  
         iframe.style.position = 'fixed';  
         iframe.style.bottom = '0px';  
         iframe.style.right = '0';  
         iframe.style.zIndex = '1000';  
-        iframe.style.width = '100%'; // Full width for chat bubble  
+      }  
+
+      if (minWidth) {  
+        iframe.style.minWidth = minWidth;  
+      }  
+      if (minHeight) {  
+        iframe.style.minHeight = minHeight;  
+      }  
+      if (maxWidth) {  
+        iframe.style.maxWidth = maxWidth;  
+      }  
+      if (maxHeight) {  
+        iframe.style.maxHeight = maxHeight;  
       }  
 
       if (this.getAttribute("style")) {  
@@ -32,22 +49,26 @@
 
       this.appendChild(iframe);  
 
-      this.updateIframeDimensions(iframe); // Set initial dimensions  
+      this.updateIframeDimensions(iframe);  
       window.addEventListener("resize", () => {  
         this.updateIframeDimensions(iframe);  
+      });  
+
+      window.addEventListener("message", (event) => {  
+        if (event.data.type === "agent-embed-resize" && event.data.width && event.data.height) {  
+          iframe.style.width = event.data.width + "px";  
+          iframe.style.height = event.data.height + "px";  
+        }  
       });  
     }  
 
     updateIframeDimensions(iframe) {  
       const viewportWidth = window.innerWidth;  
 
-      // Responsive width and height based on screen size  
       if (viewportWidth < 640) {  
-        // Small screen adjustments  
         iframe.style.width = "100%";  
         iframe.style.height = "70vh";  
       } else {  
-        // Larger screen adjustments  
         iframe.style.width = "400px";  
         iframe.style.height = "600px";  
       }  
